fddfd455b773cd27abea788608f5db11115eccb6
Gabriel Piva <89921667+gabriel-piva@users.noreply.github.com>
2023-05-16 14:39:28 -0300

    classes

--- js/character.js
+++ js/character.js
@@ -30,13 +30,79 @@ export { Attribute, Skill };
 
 // Attack & Ability
 
-class Attack {}
-class Ability {}
+class Attack {
+    name;
+    cost;
+    damage;
+    description;
+    constructor(name, cost, damage, description) {
+        this.name = name;
+        this.cost = cost;
+        this.damage = damage;
+        this.description = description;
+    }
+}
+class Ability {
+    name;
+    cost;
+    description;
+    constructor(name, cost, description) {
+        this.name = name;
+        this.cost = cost;
+        this.description = description;
+    }
+}
+
+export { Attack, Ability };
+
+// --------------------------------------------------------------------------
+
+// Stats (Life, Sanity, Power)
+
+class Stat {
+    current;
+    max;
+    constructor(current, max) {
+        this.current = current;
+        this.max = max;
+    }
+}
 
 // --------------------------------------------------------------------------
 
 // Character
 
-class Character {}
+class Character {
+    name; attributes; skills;
+    image; life; sanity; power; 
+    class; race; description; level;
+    defense; armor; condition; movement; 
+    initiative; attacks; abilities;
+    inventory; notes;
+    constructor(name, attributes, skills) {
+        this.name = name;
+        this.attributes = attributes;
+        this.skills = skills;
+        this.image = "../images/starter.jpg";
+        this.life = new Stat(0, 0);
+        this.sanity = new Stat(0, 0);
+        this.power = new Stat(0, 0);
+        this.class = "";
+        this.race = "";
+        this.description = "";
+        this.level = "";
+        this.defense = "";
+        this.armor = "";
+        this.condition = "";
+        this.movement = "";
+        this.initiative = "";
+        this.attacks = new Array();
+        this.abilities = new Array();
+        this.inventory = new Array();
+        this.notes = "";
+    }
+}
+
+export { Character };
 
 // --------------------------------------------------------------------------
